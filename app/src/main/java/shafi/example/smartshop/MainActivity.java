3b08337e5ceba6b02a1e59f8346bf697bcb5b5ca
shafi <jonyszone@gmail.com>
2020-10-11 03:06:04 +0600

    Initial commit

---
+++ app/src/main/java/shafi/example/smartshop/MainActivity.java
@@ -0,0 +1,22 @@
+package shafi.example.smartshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button joinNowButton, loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        joinNowButton = findViewById(R.id.main_join_btn);
+        loginButton = findViewById(R.id.main_login_btn);
+
+        
+    }
+}
