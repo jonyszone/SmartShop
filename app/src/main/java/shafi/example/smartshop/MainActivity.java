cfc9da4bb67de55b5ee996e2e004363504d7ecf0
shafi <jonyszone@gmail.com>
2020-10-14 01:45:42 +0600

    layout changed

--- app/src/main/java/shafi/example/smartshop/MainActivity.java
+++ app/src/main/java/shafi/example/smartshop/MainActivity.java
@@ -2,7 +2,9 @@ package shafi.example.smartshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
         joinNowButton = findViewById(R.id.main_join_btn);
         loginButton = findViewById(R.id.main_login_btn);
 
-        
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
