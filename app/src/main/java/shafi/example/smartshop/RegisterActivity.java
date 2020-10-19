a05881dddd92396ea33237a6b4e3547e38e32bea
shafi <jonyszone@gmail.com>
2020-10-19 12:30:31 +0600

    added join button

---
+++ app/src/main/java/shafi/example/smartshop/RegisterActivity.java
@@ -0,0 +1,26 @@
+package shafi.example.smartshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private Button CreateAccountButton;
+    private EditText InputName, InputPhoneNumber, InputPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        CreateAccountButton = findViewById(R.id.register_btn);
+        InputName = findViewById(R.id.register_username_input);
+        InputPhoneNumber = findViewById(R.id.register_phone_number_input);
+        InputPassword = findViewById(R.id.register_password_input);
+
+
+    }
+}
