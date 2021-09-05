f69835fd6349fe298c5efdafd616fbfd37003d96
jonyszone <jonyszone@gmail.com>
2021-09-05 22:28:24 +0600

    changed some color attributes

--- app/src/main/java/shafi/example/smartshop/AdminAddNewProductActivity.java
+++ app/src/main/java/shafi/example/smartshop/AdminAddNewProductActivity.java
@@ -3,15 +3,27 @@ package shafi.example.smartshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
     private String CategoryName;
+    private Button AddNewProduct;
+    private ImageView InputProductImage;
+    private EditText InputProductName, InputProductDescription, InputProductPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_new_product);
+
+        AddNewProduct = findViewById(R.id.add_new_product);
+        InputProductImage = findViewById(R.id.select_product);
+        InputProductName = findViewById(R.id.product_name);
+        InputProductDescription = findViewById(R.id.product_description);
+        InputProductPrice = findViewById(R.id.product_price);
 
         CategoryName = getIntent().getExtras().get("category").toString();
         Toast.makeText(this, CategoryName, Toast.LENGTH_SHORT).show();
