a73dbba8907fdb9ac471322bf12b6312c565e6c1
jonyszone <jonyszone@gmail.com>
2021-09-28 10:12:15 +0600

    added firebase push notification

--- app/src/main/java/shafi/example/smartshop/AdminAddNewProductActivity.java
+++ app/src/main/java/shafi/example/smartshop/AdminAddNewProductActivity.java
@@ -10,9 +10,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
-
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
